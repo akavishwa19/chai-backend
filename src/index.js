e18a9fe129f71f4a82c9c90e74bcfca1b97143c4
Akash <akash19102001@gmail.com>
2024-01-03 14:40:19 +0530

    getting into it

--- src/index.js
+++ src/index.js
@@ -1,13 +1,22 @@
 
 // import express from "express";
 // const app=express();
-import connectDB from "./db/index.js";
 // require('dotenv').config()
+import app from './app.js'
+import connectDB from "./db/index.js";
 import dotenv from "dotenv";
 dotenv.config();
 
 
-connectDB();
+connectDB()
+.then(()=>{
+    app.listen(process.env.PORT || 8000,()=>{
+        console.log('server running on http://localhost:'+process.env.PORT || 8000);
+    })
+})
+.catch((err)=>{
+    console.log('mongo connection error',err);
+});
 
 
 /*

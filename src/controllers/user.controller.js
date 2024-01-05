1108cc7ee9f5ea82baa46031b77647aa6295aaf5
Akash <akash19102001@gmail.com>
2024-01-06 05:16:14 +0530

    aggregation query

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -253,7 +253,7 @@ const updateAccountDetails= asyncHandler(  async (req,res)=>{
         throw new ApiError(400,"invalid details")
     }
 
-    const user=User.findByIdAndUpdate(
+    const user=await User.findByIdAndUpdate(
         req.user._id,
         {
             $set:{
@@ -342,6 +342,76 @@ const updateUserCoverImage= asyncHandler(async (req,res)=>{
             user,
             "cover image updated successfully"
         )
+    )
+})
+
+const getUserChannelProfile= asyncHandler( async (req,res)=>{
+    const {username} = req.params
+    if(!username?.trim()){
+        throw new ApiError(400,"username is missing")
+    }
+    const channel=await User.aggregate[
+        {
+            $match:{
+                username:username?.toLowerCase()
+            }
+        },
+        {
+            $lookup:{
+                from:"Subscription",
+                localField:"_id",
+                foreignField:"channel",
+                as:"subscribers"
+            }
+        },
+        {
+            $lookup:{
+                from:"Subscription",
+                localField:"_id",
+                foreignField:"subscriber",
+                as:"subscribedTo"
+            }
+        },
+        {
+            $addFields:{
+                subscribersCount:{
+                    $size:"$subscribers"
+                },
+                channelsSubscribedToCount:{
+                    $size:"$subscribedTo"
+                },
+                isSubscribed:{
+                    $cond:{
+                        if:{$in:[req.user?._id,"$subscribers.subscriber"]},
+                        then:true,
+                        else:false
+                    }
+                }
+            }
+        },
+        {
+            $project:{
+                fullName:1,
+                username:1,
+                subscribersCount:1,
+                channelsSubscribedToCount:1,
+                isSubscribed:1,
+                avatar:1,
+                coverImage:1,
+                email:1
+            }
+        }
+    ]
+    if(!channel?.length){
+        throw new ApiError(400,"channel does not exist")
+    }
+    console.log(channel);
+    return res
+    .status(200)
+    .json(
+        new ApiResponse(200,
+            channel[0],
+            "user channel etched successfully")
     )
 })
 

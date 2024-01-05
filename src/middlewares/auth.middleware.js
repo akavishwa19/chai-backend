2280e3f9be18feb76f300e8040903fa8d5f647c8
Akash <akash19102001@gmail.com>
2024-01-06 00:51:31 +0530

    logout

---
+++ src/middlewares/auth.middleware.js
@@ -0,0 +1,25 @@
+import {asyncHandler} from "../utils/asyncHandler.js";
+import {ApiError} from "../utils/apiError.js";
+import jwt from "jsonwebtoken";
+import {User} from "../models/user.model.js"
+
+const verifyJWT=asyncHandler(async (req,_,next)=>{
+    try {
+        const token=req.cookies?.accessToken || req.header("Authorization")?.replace("Bearer ","")
+        if(!token){
+            throw new ApiError(400,"unauthorized request")
+        }
+        const decodedToken= jwt.verify(token,process.env.ACCESS_TOKEN_SECRET);
+    
+        const user=await User.findById(decodedToken?._id).select("-password -refreshToken")
+        if(!user){
+            throw new ApiError(400,"invalid token");
+        }
+        req.user=user;
+        next();
+    } catch (error) {
+        throw new ApiError(400,"invalid token")
+    }
+})
+
+export {verifyJWT}
